f1bc3e1a77089f3978cdb95c750af01d6eb74912
Shaun O'Connell <ndorfin@gmail.com>
2021-05-02 15:49:04 +1200

    Move data into a  and  global object

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,5 @@ const yaml = require("js-yaml");
 module.exports = eleventyConfig => {
   
   eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
-  
-  eleventyConfig.addNunjucksFilter( 'json', obj => JSON.stringify(obj));
 
 };
